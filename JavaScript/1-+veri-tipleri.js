1615dd6a1e0c20fe5ee423bcfc96c74066696b96
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-09-19 22:06:24 +0200

    veri tipleri dosyası güncellendi

--- JavaScript/1-+veri-tipleri.js
+++ JavaScript/1-+veri-tipleri.js
@@ -7,6 +7,7 @@
 //  bu şekilde de tek satırlı yorum yazılabilir
 // cümlelerin sonu ; ile bitse iyi olur ama bitmese de çalışır.
 // kodları satır satır yazıyoruz
+// uzun metinler çift tırnak kısa veriler tek tırnak yapılıyor. ama olmasa da olur
 
 var a = 11 ;
 var b = 22  ;
@@ -17,26 +18,65 @@ console.log(a+b+c);
 // JS iki tür veri Tipi var.   1-Primitive  2- Referance Veri Tipleri (JS de object olarak gözüken tiplerin tamamı referance veri tipidir )
 // farkı : primitive sadece o değer için geçerlidir. ama referance ise o bellekteki yerine gider ve değer güncellenir
 
-// 1-Primitive 
-// a- Number Veri Tipi
+// 1-PRİMİTİVE 
+
+// a- NUMBER Veri Tipi
+// JS de float vs yoktur sadece number vardır
+
 var a = 11 ; 
 var b = 3.14 ;
 console.log(a,b);
 console.log(typeof(a));
+çıktısı : 
+11 3.14
+number
 
-// b- String Veri Tipi
+
+örnek : 
+
+var myNumber = -10;
+console.log("myNumber: " + myNumber);
+çıktı : 
+myNumber: -10
+
+örnek : 
+
+const x = 123
+console.log(` x : ${x} \n typeof : ${x}`)
+çıktısı : 
+x : 123 
+typeof : 123
+
+
+// b- STRING Veri Tipi
 
 var name = "talha" ;
 var talha = "yakışıklı" ; 
-var eyüp = "karizma"  ;
-var yusuf = "artist"  ;
 var kamil = "cool"  ;
 
-console.log (typeof(name));
-console.log (name);
-console.log (talha,yusuf,eyüp,kamil);
+örnek : 
 
-// c- Boolean Veri Tipi
+console.log (typeof(name));
+// name kısmını paranteze almasak da olur
+console.log (typeof name);
+console.log (name);
+console.log (talha,kamil);
+çıktı : 
+String
+talha
+yakışıklı cool
+
+örnek : 
+
+var yaz = "123"
+console.log (typeof(yaz));
+console.log (yaz);
+çıktı : 
+String
+123
+
+// c- BOOLEAN Veri Tipi
+// true veya false olarak sonuç verir. küçük harf ile yazmak lazım.
 
 var a = true ;
 var b= false; // büyük harf ile yazdığın zaman sonuç çıkmıyor
@@ -44,13 +84,60 @@ console.log(a) ;
 console.log(b) ;
 console.log(typeof a) ;
 
-// d- Null
+örnek :
+
+const myBool = 8 > 2
+const myCool = 1>8
+console.log(myBool)
+console.log(myCool)
+console.log(typeof myBool)
+console.log(typeof myCool)
+çıktı : 
+true
+false
+Boolean
+Boolean
+
+örnek : 
+
+const myBool3 = 12 == 12;
+const myBool4 = 12 == 13;
+// eşit olup olmadığını anlamak için 2 eşittir kullanılır. 
+console.log(myBool3)
+console.log(myBool4)
+console.log(typeof myBool3)
+console.log(typeof myBool4)
+
+örnek : 
+
+const myBool3 = 12;
+const myBool4 = "12";
+const myBool = myBool3 === myBool4
+// burada 3 tane eşittir yan yana kullanılmış. bu aynı zaman da veri tipi de eşit midir değil midir onu da  kontrol eder. mesela bunun sonucu false çıkar . Ama burada iki eşittir olsa idi True çıkacaktı.
+console.log(myBool)
+
+örnek :
+
+myVar_1 = Boolean(0) ; 
+myVar_2 = Boolean("") ;
+myVar_3 = Boolean(-0) ;
+myVar_4 = null ;
+var s ;
+// bu çıktıların tamamı false çıkar
+console.log("x : " + myVar_1)
+console.log("x : " + myVar_2)
+console.log("x : " + myVar_3)
+console.log("x : " + Boolean(myVar_4) )
+console.log("x : " + Boolean(s))
+
+// d- NULL
 
 var a = null ;
 console.log(a) ;
 console.log(typeof(a)); // type olarak object çıkar. bu  bir bug'dır. esasında bu bir hata gibi. Null hiçbirşey taşımadığını belirten bir değerdir.
 
-// d- Undefined
+// d- UNDEFİNED
+// bu bir hatadır. aslında tanımlanmayan veri demektir
 
 var a = 
 console.log(a) ;
@@ -66,7 +153,7 @@ var names = "ahmet"
 console.log (names[0])
 
 
-// 2- Referance Veri Tipleri 
+// 2- REFERANCE Veri Tipleri 
 
 var person = {
     name : "talha",
@@ -92,3 +179,17 @@ console.log(typeof merhaba) ;
 console.warn("bu bir uyarıdır.") // bu şekilde hata veya ikaz yarısı yaptırılabilir.
 console.error("bu sayfa bulunamadı") // bu şekilde hata veya ikaz yarısı yaptırılabilir.
 // console.log(clear); // bu şekilde console temizlenebilir
+
+// 3- EKSTRA
+
+örnek : 
+
+const myValue1 = 10;
+const myValue2 = "7";
+console.log(`myValue1 + myvalue2 : ${myValue1 + myValue2}`)
+// burada sonuç string olarak çıkar ve yan yana yazar
+console.log(`myValue1 + myvalue2 : ${myValue1 - myValue2}`)
+// burada normal bir matematik işlemi yapıuyor
+çıktı
+myValue1 + myvalue2 : 107 typeof : String
+myValue1 + myvalue2 : 3    typeof : Number
